74b4ec2eb88402f157e345ae152b50cbcec5a3fe
PandaGab <gabriel.leclerc.2@ulaval.ca>
2021-04-08 15:50:00 -0400

    Moved zshrc's command specific to compute server in zshrc

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -3,9 +3,6 @@
 # Install Oh-My-Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-# On compute server, might need to had this path to grant access to zsh functions
-# export FPATH="/cvmfs/soft.computecanada.ca/nix/var/nix/profiles/16.09/share/zsh/5.2/functions:$FPATH"
-
 # Symlinks dotfiles in the user space
 DOTFILES=(.zshrc .custom_aliases .custom_functions .gitconfig .tmux.conf)
 for dotfile in $DOTFILES; do
