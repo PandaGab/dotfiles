f7f7a405894af0657e16dad5b126205973c0ea77
PandaGab <gabriel.leclerc.2@ulaval.ca>
2021-04-08 15:38:53 -0400

    Check if config exists before trying to link it

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -6,12 +6,12 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 # On compute server, might need to had this path to grant access to zsh functions
 # export FPATH="/cvmfs/soft.computecanada.ca/nix/var/nix/profiles/16.09/share/zsh/5.2/functions:$FPATH"
 
-# Installation will create a zshrc config file, we will remove it
-rm "${PWD}/.zshrc"
-
 # Symlinks dotfiles in the user space
-ln -s "${PWD}/.custom_aliases" $HOME/.custom_aliases
-ln -s "${PWD}/.custom_functions" $HOME/.custom_functions
-ln -s "${PWD}/.tmux.conf" $HOME/.tmux.conf
-ln -s "${PWD}/.zshrc" $HOME/.zshrc
-ln -s "${PWD}/.gitconfig" $HOME/.gitconfig
+DOTFILES=(.zshrc .custom_aliases .custom_functions .gitconfig .tmux.conf)
+for dotfile in $DOTFILES; do
+    if [[ -f ${PWD}/$dotfile ]]; then
+        echo "${PWD}/$dotfile already exists, remove it manually"
+    else
+        ln -s "${PWD}/$dotfile" $HOME/$dotfile
+    fi
+done
