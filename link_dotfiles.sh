4d477a44a58a731a41ce4e07d8c1baadb248f1aa
PandaGab <gabriel.leclerc.2@ulaval.ca>
2021-04-08 12:54:50 -0400

    Added oh-my-zsh installation

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # Symlinks dotfiles in the user space
-#ln -s "${PWD}/.custom_aliases" $HOME/.custom_aliases
-#ln -s "${PWD}/.custom_functions" $HOME/.custom_functions
-#ln -s "${PWD}/.tmux.conf" $HOME/.tmux.conf
-#ln -s "${PWD}/.zshrc" $HOME/.zshrc
-#ln -s "${PWD}/.gitconfig" $HOME/.gitconfig
+ln -s "${PWD}/.custom_aliases" $HOME/.custom_aliases
+ln -s "${PWD}/.custom_functions" $HOME/.custom_functions
+ln -s "${PWD}/.tmux.conf" $HOME/.tmux.conf
+ln -s "${PWD}/.zshrc" $HOME/.zshrc
+ln -s "${PWD}/.gitconfig" $HOME/.gitconfig
 
 # Install Oh-My-Zsh
 curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
