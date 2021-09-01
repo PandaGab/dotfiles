1c8064aba4d111bd692f1811c793f8830834dcef
PandaGab <gabriel.leclerc.2@ulaval.ca>
2021-09-01 15:41:09 -0400

    Moved user settings in home folder

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -1,14 +1,53 @@
 #!/bin/sh
 
-# Install Oh-My-Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+# # Install Oh-My-Zsh
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-# Symlinks dotfiles in the user space
-DOTFILES=(.zshrc .custom_aliases .custom_functions .gitconfig .tmux.conf)
-for dotfile in $DOTFILES; do
-    if [[ -f $HOME/$dotfile ]]; then
-        echo "$HOME/$dotfile already exists, remove it manually"
+#######################
+# Link user settings
+#######################
+for FILE in $(ls -A1 home); do
+    FILEPATH=$HOME/$FILE
+    if [[ -L $FILEPATH ]] || [[ -f $FILEPATH ]]; then
+        echo "$HOME/$FILE already exists."
+        read -p "Do you want to replace it? [y/n]" -n 1 -r
+        echo # move to a new line
+        if [[ $REPLY =~ ^[Yy]$ ]]; then
+            rm "$HOME/$FILE"
+            ln -s "${PWD}/home/$FILE" "$HOME/$FILE"
+        fi
     else
-        ln -s "${PWD}/$dotfile" $HOME/$dotfile
+        echo not there
+        #ln -s "${PWD}/home/$FILE" "$HOME/$FILE"
     fi
 done
+
+#######################
+# Link vscode settings
+#######################
+OS=$(uname)
+if [ $OS == "Linux" ]; then
+    VSCODE_USER_SETTINGS_FOLDER="$HOME/.config/Code/User"
+elif [ $OS == "Darwin" ]; then
+    VSCODE_USER_SETTINGS_FOLDER="$HOME/Library/Application Support/Code/User"
+fi
+
+if [[ -d $VSCODE_USER_SETTINGS_FOLDER ]]; then
+    for SETTINGFILE in $(ls -A1 vscode); do
+        SETTINGPATH="$VSCODE_USER_SETTINGS_FOLDER/$SETTINGFILE"
+        if [[ -L $SETTINGPATH ]] || [[ -f $SETTINGPATH ]]; then
+            echo "$SETTINGPATH already exists."
+            read -p "Do you want to replace it? [y/n]" -n 1 -r
+            echo    # move to a new line
+            if [[ $REPLY =~ ^[Yy]$ ]]; then
+                rm "$SETTINGPATH"
+                ln -s "${PWD}/vscode/$SETTINGFILE" "$SETTINGPATH"
+            fi
+        else
+            ln -s "${PWD}/vscode/$SETTINGFILE" "$SETTINGPATH"
+        fi
+    done
+else
+    echo "Can't link vscode settings."
+    echo "$VSCODE_USER_SETTINGS_FOLDER not found"
+fi
